c8cae593526bc1b555d905c8836965565c423f50
Landon Bouma <landonb@lethe>
2023-12-11 20:30:57 -0600

    Improve: Use full paths so aliases work from derived projects

--- home/.config/ohmyrepos/my-deps-manage-easy-as-pypi.sh
+++ home/.config/ohmyrepos/my-deps-manage-easy-as-pypi.sh
@@ -169,11 +169,11 @@ infuse_easy_as_pypi_follower_links () {
 infuse_easy_as_pypi_git_aliases () {
   # Project: https://github.com/landonb/git-bump-version-tag
   # - Easily apply a semantic version tag.
-  git config alias.bump "! bin/git-bump-version-tag"
+  git config alias.bump "! ${DOPP_KIT:-${HOME}/.kit}/py/easy-as-pypi/bin/git-bump-version-tag"
 
   # Project: https://github.com/landonb/git-bump-version-tag
   # - Easily apply a semantic version tag.
-  git config alias.cascade "! bin/git-cascade"
+  git config alias.cascade "! ${DOPP_KIT:-${HOME}/.kit}/py/easy-as-pypi/bin/git-cascade"
 }
 
 # ***
