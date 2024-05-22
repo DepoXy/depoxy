9bdb2c514988bb9931c03c6b06f2cc24fbf69901
Landon Bouma <landonb@users.noreply.github.com>
2024-05-22 01:50:38 -0500

    Docs: Adjust comment

--- home/.kit/git/ohmyrepos/lib/append-bash-completion.sh
+++ home/.kit/git/ohmyrepos/lib/append-bash-completion.sh
@@ -3,13 +3,18 @@
 # Project: https://github.com/DepoXy/depoxy#🍯
 # License: MIT
 
-# CXREF/2024-04-03:
-#   ~/.kit/git/ohmyrepos/lib/line-in-file.sh
+# CXREF/2023-05-14: ~/.bash_completion is sourced by /etc/bash_completion
+#   /etc/bash_completion -> /usr/share/bash-completion/bash_completion
+#   # @macOS:
+#   /opt/homebrew/share/bash-completion/bash_completion
+#     -> ../../Cellar/bash-completion@2/2.14.0/share/bash-completion/bash_completion
 append_bash_completion () {
   local add_line="$1"
 
   local target_path="${HOME}/.bash_completion"
 
+  # CXREF/2024-04-03:
+  #   ~/.kit/git/ohmyrepos/lib/line-in-file.sh
   line_in_file "${add_line}" "${target_path}"
 }
 
