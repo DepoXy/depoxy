7bcf9121daa4f23b1a2192f8a6a44bf51b1ee75d
Landon Bouma <landonb@users.noreply.github.com>
2024-06-14 23:52:32 -0500

    Wind up: Wipe tedious ~/.projlns generator (“You're too complicated”)
    - We'll replace the brittle, file-by-file ~/.projlns/omr generator
      with a `find` solution, next.
    
      Split into two commits because it'll be all new code, easier to
      suss that way.

--- home/.projlns/infuse-projlns-omr.sh
+++ home/.projlns/infuse-projlns-omr.sh
@@ -23,11 +23,6 @@ source_deps () {
   # Load: link_deep, and remove_symlink_hierarchy_safe. Requires logger.sh.
   # CXREF: ~/.kit/git/myrepos-mredit-command/lib/link_deep.sh
   . "${GITREPOSPATH:-${HOME}/.kit/git}/myrepos-mredit-command/lib/link_deep.sh"
-
-  # Load: _vendorfs_path_running_client_print.
-  # - CXREF: ~/.depoxy/ambers/core/depoxy_fs.sh
-  local ambers_path="${DEPOXYDIR_BASE_FULL:-${HOME}/.depoxy}/ambers"
-  . "${DEPOXYAMBERS_DIR:-${ambers_path}}/core/depoxy_fs.sh"
 }
 
 # +++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++ #
@@ -42,127 +37,7 @@ infuse_projects_links_omr_config () {
   info "$(fg_mintgreen)$(attr_emphasis)Creating links$(attr_reset)" \
     "$(fg_lightorange)$(pwd)$(attr_reset)"
 
-  infuse_create_symlinks_omr_home
-
-  infuse_create_symlinks_omr_ohmyrepos
-
-  infuse_create_symlinks_omr_vim
-
-  infuse_create_symlinks_omr_kit_root
-
-  infuse_create_symlinks_omr_kit_projects
-
-  infuse_create_symlinks_omr_client
-
-  infuse_create_symlinks_omr_stints
-
   cd "${before_cd}"
-}
-
-# +++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++ #
-
-# *** ${HOME}
-
-infuse_create_symlinks_omr_home () {
-  local ambers_path="${DEPOXYDIR_BASE_FULL:-${HOME}/.depoxy}/ambers"
-  local home_cfg_root="${DEPOXYAMBERS_DIR:-${ambers_path}}/home"
-
-  link_deep "${home_cfg_root}/_mrconfig"
-}
-
-# *** ${GITREPOSPATH}/ohmyrepos/.mrconfig-omr
-
-infuse_create_symlinks_omr_ohmyrepos () {
-  local omr_cfg_root="${OHMYREPOS_DIR:-${GITREPOSPATH:-${HOME}/.kit/git}/ohmyrepos}"
-
-  link_deep "${omr_cfg_root}/.mrconfig-omr"
-  # Just FYI (we don't need to put it under search), the example config:
-  #  link_deep "${GITREPOSPATH:-${HOME}/.kit/git}/ohmyrepos/.mrconfig.example"
-}
-
-# *** ${HOME}/.vim
-
-infuse_create_symlinks_omr_vim () {
-  local ambers_path="${DEPOXYDIR_BASE_FULL:-${HOME}/.depoxy}/ambers"
-  local vim_cfg_root="${DEPOXYAMBERS_DIR:-${ambers_path}}/home/.vim"
-
-  link_deep "${vim_cfg_root}/_mrconfig"
-  link_deep "${vim_cfg_root}/_mrconfig-3rdp"
-  link_deep "${vim_cfg_root}/_mrconfig-dubs"
-  link_deep "${vim_cfg_root}/_mrconfig-lsp"
-}
-
-# *** ${DOPP_KIT:-${HOME}/.kit}
-
-infuse_create_symlinks_omr_kit_root () {
-  local ambers_path="${DEPOXYDIR_BASE_FULL:-${HOME}/.depoxy}/ambers"
-  local kit_cfg_root="${DEPOXYAMBERS_DIR:-${ambers_path}}/home/.kit"
-
-  link_deep "${kit_cfg_root}/_mrconfig"
-}
-
-# *** ${DOPP_KIT:-${HOME}/.kit}/**
-
-infuse_create_symlinks_omr_kit_projects () {
-  local ambers_path="${DEPOXYDIR_BASE_FULL:-${HOME}/.depoxy}/ambers"
-  local kit_cfg_root="${DEPOXYAMBERS_DIR:-${ambers_path}}/home/.kit"
-
-  link_deep "${kit_cfg_root}/git/_mrconfig-git-core"
-  link_deep "${kit_cfg_root}/git/_mrconfig-git-smart"
-  link_deep "${kit_cfg_root}/go/_mrconfig"
-  link_deep "${kit_cfg_root}/js/_mrconfig"
-  link_deep "${kit_cfg_root}/mOS/_mrconfig"
-  link_deep "${kit_cfg_root}/mOS/_mrconfig-excess"
-  link_deep "${kit_cfg_root}/odd/_mrconfig"
-  link_deep "${kit_cfg_root}/py/_mrconfig"
-  link_deep "${kit_cfg_root}/sh/_mrconfig-bash"
-  link_deep "${kit_cfg_root}/sh/_mrconfig-shell"
-  link_deep "${kit_cfg_root}/txt/_mrconfig"
-}
-
-# +++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++ #
-
-# *** ${DEPOXYDIR_STINTS_FULL:-${HOME}/.depoxy/stints}/${DEPOXY_CLIENT_ID}
-#
-#   Look for *mrconfig* in current Client directory, e.g., ~/.depoxy/stints/2321/
-
-# THOTS: How best to while-loop.
-#
-# - Is there a preferred way to loop over `find` results?
-#   Is one more POSIX-compliant, or backwards-compatible?
-#
-# - Here's a loop using a here document:
-#
-#     while read -r mrconfig_path; do
-#       echo "mrconfig_path=$mrconfig_path"
-#     done <<< $(find "${client_basedir}" -type f -regex '.*mrconfig.*')
-#
-# But perhaps putting the find before the while is more readable.
-
-infuse_create_symlinks_omr_client () {
-  local client_basedir
-  client_basedir="$(_vendorfs_path_running_client_print)" || return 0
-
-  local mrconfig_path
-  while read -r mrconfig_path; do
-    link_deep "${mrconfig_path}"
-  done < <(find "${client_basedir}" -type f -regex '.*mrconfig.*')
-}
-
-# ***
-
-# Optional ~/.depoxy/stints/_mrconfig
-infuse_create_symlinks_omr_stints () {
-  local stints_basedir
-  stints_basedir="$(_vendorfs_path_stints_basedir_print)" || return 0
-  # ALTLY:
-  #   client_basedir="${DEPOXYDIR_STINTS_FULL:-${HOME}/.depoxy/stints}"
-
-  local mrconfig_path="${stints_basedir}/_mrconfig"
-
-  if [ -f "${mrconfig_path}" ]; then
-    link_deep "${mrconfig_path}"
-  fi
 }
 
 # @@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@ #
