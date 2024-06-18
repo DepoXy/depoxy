d0c4e319bd89c82d3ba2bd2d9fc92ca1b93a033e
Landon Bouma <landonb@users.noreply.github.com>
2024-06-18 00:54:57 -0500

    Update: UX: Add context to trace

--- home/.kit/git/ohmyrepos/lib/depoxy-client-print-home-shadow-project.sh
+++ home/.kit/git/ohmyrepos/lib/depoxy-client-print-home-shadow-project.sh
@@ -46,11 +46,11 @@ order = ${mr_order}
 skip = mr_exclusive "home"
 lib =
   use_remote_home_if_depoxy_client_matches "${client_id}"
-  debug "MR_REMOTE_PATH: \${MR_REMOTE_PATH}"
+  debug "MR_REMOTE_PATH: \${MR_REMOTE_PATH} [lib:depoxy-client-print-home-shadow-project.sh]"
   remote_set_private "\$(basename -- "\${MR_DXC_HOME_SHADOW:-home/.home}")"
 ffssh =
   use_remote_home_if_depoxy_client_matches "${client_id}"
-  debug "MR_REMOTE_PATH: \${MR_REMOTE_PATH}"
+  debug "MR_REMOTE_PATH: \${MR_REMOTE_PATH} [ffssh:depoxy-client-print-home-shadow-project.sh]"
   git_merge_ffonly_ssh_mirror "\$@"
 
 [DEFAULT]
