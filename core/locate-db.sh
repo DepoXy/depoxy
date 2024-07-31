7249098645a1f4d8b1d0c0b90698868814a14ff6
Landon Bouma <landonb@users.noreply.github.com>
2024-07-31 00:15:22 -0500

    Improve: Add mlocate and plocate support for db on fuse path

--- core/locate-db.sh
+++ core/locate-db.sh
@@ -38,12 +38,44 @@ _hf_locate () {
     echo "CHORE: Please install \`plocate\` (Linux) or \`glocate\` (macOS)"
   fi
 
-  if [ -f "${db_path}" ]; then
-    command ${locate_cmd} --database "${db_path}" "$@"
-  else
+  if [ ! -f "${db_path}" ]; then
     echo "CHORE: Please create (or mount) ${db_path} (or set LOCATEDB_PATH)"
 
     command ${locate_cmd} "$@"
+
+    return $?
+  fi
+
+  # On Linux, if the database is on a gocryptfs path (or possibly any
+  # fuse mount), mlocate and plocate fail, "Permission denied".
+  # - At least mlocate can take db on stdin.
+  # - But plocate cannot take it on the in. And don't hope you can use
+  #   process substitution instead, because <(cat ${db_path}) also fails,
+  #   complaining, "pread: Illegal seek".
+  # REFER: E.g., mlocate --version  # mlocate 0.26
+  #              plocate --version  # plocate 1.1.15
+  #              glocate --version  # locate (GNU findutils) 4.10.0
+  if ! \
+    command ${locate_cmd} --database "${db_path}" "$@" 2> /dev/null \
+  ; then
+    local locate_vers
+    locate_vers="$(command ${locate_cmd} --version)"
+
+    if [[ "${locate_vers}" = "mlocate"* ]]; then
+      cat "${db_path}" | command ${locate_cmd} -d- "$@"
+    elif [[ "${locate_vers}" = "plocate"* ]]; then
+      # OPSEC/2024-07-31: Not the perfectist approach, but the command
+      # is still fast. Alternative is for user to move the db off the fuse.
+      local tmp_db="$(mktemp --suffix=".db" --tmpdir "_hf_locate-XXXXXXX")"
+      command cp -- "${db_path}" "${tmp_db}"
+      command ${locate_cmd} --database "${tmp_db}" "$@"
+      command rm -- "${tmp_db}"
+    else
+      # Probably glocate, which can access db on APFS path.
+      # - So unexpected path.
+      # Run again, this time with stderr.
+      command ${locate_cmd} --database "${db_path}" "$@"
+    fi
   fi
 }
 
