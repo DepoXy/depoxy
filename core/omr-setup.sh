3cb3e57f4af075c065f7d186786a2011c3d5d439
Landon Bouma <landonb@users.noreply.github.com>
2024-10-30 15:08:02 -0500

    Update: UX: Add `sudo -v` to post-macOS update `infuse` reminder
    - Also add similar copy-pasta to `infuse-platform-macOS` message.

--- core/omr-setup.sh
+++ core/omr-setup.sh
@@ -80,11 +80,12 @@ _dxy_check_if_omr_infuse_run_since_latest_macos_update () {
     # Reset cursor in case HOMEFRIES_LOADINGDOTS.
     printf '\r'
     echo "$(attr_emphasis)Please run$(attr_reset)" \
-      "\`$(attr_bold)infuse$(attr_reset)\`" \
-      "$(attr_emphasis)because$(attr_reset)" \
-      "$(attr_underline)macOS recently updated$(attr_reset) 🔔"
-    echo "              三"
-    echo "              三                     ᕕ( ᐛ )ᕗ"
+      "\`$(attr_bold)sudo -v; infuse$(attr_reset)\`" \
+      "$(attr_emphasis)given that$(attr_reset) macOS $(attr_emphasis)was $(attr_reset)"
+    echo "          " \
+      "   三       三   " \
+      "$(attr_underline)recently updated$(attr_reset) 🔔"
+    echo "              三       三        ᕕ( ᐛ )ᕗ"
   fi
 }
 
