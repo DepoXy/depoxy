8825a98b4d3f621ab4c6e1e83b44d6a092c2ff31
Landon Bouma <landonb@users.noreply.github.com>
2024-11-14 23:21:50 -0600

    Update: SH FZF: Desimplify file globs
    - So that we can specify exact file names, e.g., ".bash_history".

--- core/fzf-setup.sh
+++ core/fzf-setup.sh
@@ -223,27 +223,27 @@ main () {
     dglobs+=(".gnupg/private-keys-v1.d")
     dglobs+=(".zsh_sessions")
     # Vim's ~/.vim_backups/*.swp
-    fglobs+=("swp")
+    fglobs+=("*.swp")
     dglobs+=(".vim_backups")
     # Graphics, Document, and other files (you probably won't open in text)
-    fglobs+=("bin")
-    fglobs+=("gif")
-    fglobs+=("gpg")
-    fglobs+=("jpg")
-    fglobs+=("Jpg")
-    fglobs+=("JPG")
-    fglobs+=("nib")
-    fglobs+=("odg")
-    fglobs+=("odt")
+    fglobs+=("*.bin")
+    fglobs+=("*.gif")
+    fglobs+=("*.gpg")
+    fglobs+=("*.jpg")
+    fglobs+=("*.Jpg")
+    fglobs+=("*.JPG")
+    fglobs+=("*.nib")
+    fglobs+=("*.odg")
+    fglobs+=("*.odt")
     # SAVVY: `rg --ignore-case` doesn't seem to work on globs, so add permutations.
-    fglobs+=("pdf")
-    fglobs+=("Pdf")
-    fglobs+=("PDF")
-    fglobs+=("png")
-    fglobs+=("pyc")
-    fglobs+=("svg")
-    fglobs+=("xpm")
-    fglobs+=("zip")
+    fglobs+=("*.pdf")
+    fglobs+=("*.Pdf")
+    fglobs+=("*.PDF")
+    fglobs+=("*.png")
+    fglobs+=("*.pyc")
+    fglobs+=("*.svg")
+    fglobs+=("*.xpm")
+    fglobs+=("*.zip")
     # DepoXy-specific directories
     dglobs+=(".crypt")
     dglobs+=(".noise/home")
@@ -277,7 +277,7 @@ main () {
           --no-ignore-vcs
           --no-ignore-parent
           -g '!**/{${dir_globs}}/**'
-          -g '!**/*.{${file_globs}}'
+          -g '!**/{${file_globs}}'
         2> /dev/null" \
       | tr -d '$\n' | sed 's/  \+/ /g' | sed 's/^ \+//'
     )"
