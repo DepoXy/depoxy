ee976c2c8ae88304a281522cbb02432cf87b6759
Landon Bouma <landonb@users.noreply.github.com>
2024-12-12 17:41:40 -0600

    Update: Vim: Add cd-aliases to vim-depoxy/plugin: `cvpd`, `cdvpd`

--- core/aliastown.sh
+++ core/aliastown.sh
@@ -280,6 +280,11 @@ _dxy_wire_aliases_pushd_paths_vim () {
   pushd_alias_or_warn "cvp" "${HOME}/.vim/pack"
   pushd_alias_or_warn "cdvp" "${HOME}/.vim/pack"
 
+  # FIXME/2024-12-12: Publish as its own plugin. (Then update this path.)
+  local dxy_plug="${DEPOXYAMBERS_DIR:-${HOME}/.depoxy/ambers}/home/.vim/pack/DepoXy/start/vim-depoxy/plugin"
+  pushd_alias_or_warn "cvpd" "${dxy_plug}"
+  pushd_alias_or_warn "cdvpd" "${dxy_plug}"
+
   # Are you a Vim plugin author? Here's a convenient pushd to your plugs.
   # - Just set the environ from your private Bashrc, e.g.,
   #     export DEPOXY_CVS_ALIAS_VIM_PLUG_ORG=yourusername
