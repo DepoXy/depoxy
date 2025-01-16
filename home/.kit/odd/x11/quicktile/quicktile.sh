f16ed86f8dda390e8678c50b4c079acf2116496a
Landon Bouma <landonb@users.noreply.github.com>
2025-01-15 20:59:02 -0600

    Bugfix: Repair paths to previously relocated quicktile

--- home/.kit/odd/x11/quicktile/quicktile.sh
+++ home/.kit/odd/x11/quicktile/quicktile.sh
@@ -1,5 +1,5 @@
 #!/bin/sh
-cd "${DOPP_KIT:-${HOME}/.kit}/odd/quicktile"
+cd "${DOPP_KIT:-${HOME}/.kit}/odd/x11/quicktile"
 venv_name=".venv-quicktile"
 . "${venv_name}/bin/activate"
 exec python3 -m quicktile "$@"
