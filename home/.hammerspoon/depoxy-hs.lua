7b67128bda71ad2c441a433f750e5f7152d56b29
Landon Bouma <landonb@users.noreply.github.com>
2025-01-30 20:54:58 -0600

    Update: Relocate ~/.vim assets under ~/.kit/nvim

--- home/.hammerspoon/depoxy-hs.lua
+++ home/.hammerspoon/depoxy-hs.lua
@@ -142,7 +142,7 @@ ignore_hotkey_slack(shift_ctrl_r)
 -- - CXREF: See Alacritty substitutions for terminal `vim`:
 --     ~/.depoxy/ambers/home/.config/alacritty/alacritty.toml
 -- - CXREF: See associated Vim maps:
---     ~/.vim/plugs/DepoXy/start/vim-depoxy/plugin/vim-shift-ctrl-bindings.vim
+--     ~/.kit/nvim/DepoXy/start/vim-depoxy/plugin/vim-shift-ctrl-bindings.vim
 
 local macvim_shift_ctrl_kludge_get_eventtap = function()
   return hs.eventtap.new(
