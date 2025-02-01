75f801a130b5b9e30d2605887af0697273e4bd14
Landon Bouma <landonb@users.noreply.github.com>
2025-02-01 11:59:10 -0600

    Insert: OMR/Vim: Add lazy.nvim plugin spec (for loading vim-trap)

---
+++ home/.kit/nvim/nvim-depoxy/.config/nvim/lua/specs/group--depoxy-client.lua
@@ -0,0 +1,46 @@
+-- vim:tw=0:ts=2:sw=2:et:ai:ft=lua
+-- Author: Landon Bouma <https://tallybark.com/>
+-- Project: https://github.com/DepoXy/depoxy#🍯
+-- THANX: https://patorjk.com/software/taag/#p=display&f=Isometric3&t=DPXYNVIM
+
+--      _____          ___         ___                       ___                                    ___
+--     /  /::\        /  /\       /__/|          ___        /__/\          ___        ___          /__/\
+--    /  /:/\:\      /  /::\     |  |:|         /__/|       \  \:\        /__/\      /  /\        |  |::\
+--   /  /:/  \:\    /  /:/\:\    |  |:|        |  |:|        \  \:\       \  \:\    /  /:/        |  |:|:\
+--  /__/:/ \__\:|  /  /:/~/:/  __|__|:|        |  |:|    _____\__\:\       \  \:\  /__/::\      __|__|:|\:\
+--  \  \:\ /  /:/ /__/:/ /:/  /__/::::\____  __|__|:|   /__/::::::::\  ___  \__\:\ \__\/\:\__  /__/::::| \:\
+--   \  \:\  /:/  \  \:\/:/      ~\~~\::::/ /__/::::\   \  \:\~~\~~\/ /__/\ |  |:|    \  \:\/\ \  \:\~~\__\/
+--    \  \:\/:/    \  \::/        |~~|:|~~     ~\~~\:\   \  \:\  ~~~  \  \:\|  |:|     \__\::/  \  \:\
+--     \  \::/      \  \:\        |  |:|         \  \:\   \  \:\       \  \:\__|:|     /__/:/    \  \:\
+--      \__\/        \  \:\       |  |:|          \__\/    \  \:\       \__\::::/      \__\/      \  \:\
+--                    \__\/       |__|/                     \__\/           ~~~~                   \__\/
+
+return {
+  -- vim-depoxy configures plugin settings (which is how author
+  -- configured plugins before adopting a proper plugin manager).
+  --
+  -- LATER/2025-02-01: Integrate vim-depoxy into nvim-depoxy
+  -- (using lazy.nvim and Lua conventions to setup maps, etc.).
+  -- - Then remove vim-depoxy here, but keep for Vim instances...
+  --   though remains to be seen how much I'll use Vim classic.
+  {
+    dir = "~/.kit/nvim/DepoXy/start/vim-depoxy",
+    lazy = not lazy_profile["vim-depoxy"],
+    dependencies = {
+      { dir = "~/.kit/nvim/embrace-vim/start/vim-fullscreen-toggle" },
+      { dir = "~/.kit/nvim/embrace-vim/start/vim-webopen" },
+    },
+  },
+
+  -- vim-trap is the user's private DepoXy Client plugin.
+  -- - The `infuse` command creates the vim-trap symlink
+  --   that targets user's ~/.depoxy/running/home/vim-trap/
+  {
+    dir = "~/.kit/nvim/DepoXy/start/vim-trap",
+    lazy = not lazy_profile["vim-trap"],
+    dependencies = {
+      dir = "~/.kit/nvim/embrace-vim/start/vim-netrw-explore-map",
+    },
+  },
+}
+
