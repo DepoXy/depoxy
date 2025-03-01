21c8fd8ee00e7f9c82b0451b732b2c1619658d36
Landon Bouma <landonb@users.noreply.github.com>
2025-02-28 23:44:44 -0600

    feat(nvim): add private nvim-lazyb config wiring

---
+++ home/.kit/nvim/landonb/nvim-lazyb/lua/config/keymaps-client.lua
@@ -0,0 +1,13 @@
+-- vim:tw=0:ts=2:sw=2:et:ai:ft=lua
+-- Author: Landon Bouma <https://tallybark.com/>
+-- Project: https://github.com/DepoXy/depoxy#🍯
+
+-- -- -- -- -- -- -- -- -- -- -- -- -- -- -- -- -- -- -- -- -- -- --
+
+-- CXREF: Optional user config:
+-- ~/.depoxy/running/home/.kit/nvim/landonb/nvim-lazyb/lua/config/keymaps-private.lua
+pcall(function()
+	require("config.keymaps-private").setup()
+end)
+
+-- -- -- -- -- -- -- -- -- -- -- -- -- -- -- -- -- -- -- -- -- -- --
---
+++ home/.kit/nvim/landonb/nvim-lazyb/lua/config/options-client.lua
@@ -0,0 +1,13 @@
+-- vim:tw=0:ts=2:sw=2:et:ai:ft=lua
+-- Author: Landon Bouma <https://tallybark.com/>
+-- Project: https://github.com/DepoXy/depoxy#🍯
+
+-- -- -- -- -- -- -- -- -- -- -- -- -- -- -- -- -- -- -- -- -- -- --
+
+-- CXREF: Optional user config:
+-- ~/.depoxy/running/home/.kit/nvim/landonb/nvim-lazyb/lua/config/options-private.lua
+pcall(function()
+	require("config.options-private").setup()
+end)
+
+-- -- -- -- -- -- -- -- -- -- -- -- -- -- -- -- -- -- -- -- -- -- --
