c676bfb601eff7330cbe96e5335cc614a526aab0
Landon Bouma <landonb@users.noreply.github.com>
2025-10-07 22:40:53 -0500

    docs(nvim): drop path crumb

--- home/.kit/nvim/landonb/nvim-lazyb/lua/config/options-client.lua
+++ home/.kit/nvim/landonb/nvim-lazyb/lua/config/options-client.lua
@@ -71,6 +71,7 @@ SetTagsProjlnsDepoxydeeplinks()
 -- -- -- -- -- -- -- -- -- -- -- -- -- -- -- -- -- -- -- -- -- -- --
 
 -- CXREF: Optional private user config (i.e., from DepoXy Client):
+--   ~/.config/nvim_lazyb/lua/config/options-private.lua
 -- ~/.depoxy/running/home/.kit/nvim/landonb/nvim-lazyb/lua/config/options-private.lua
 pcall(function()
   require("config.options-private").setup()
