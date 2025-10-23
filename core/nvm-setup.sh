53178b47cc33fcab94200b8fd84a74263884f0ba
Landon Bouma <landonb@users.noreply.github.com>
2025-10-23 18:37:30 -0500

    improve(sh): replace complicated nvm-use-latest with simpler nvm-use call

--- core/nvm-setup.sh
+++ core/nvm-setup.sh
@@ -40,39 +40,28 @@ __dxy_nvm_source_nvm_and_completion() {
 
 # +++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++ #
 
-svgo() {
-  (
-    if [ -z "$(unset -f svgo && type -p svgo)" ]; then
-      svgo_prepare || return 1
-
-      >&2 echo -e "HINT: Change env to speedup svgo: nvm use \$(nvm_latest_version)\n"
-    fi
-
-    command svgo "$@"
-  )
+_dxy_nvm_use_latest_node() {
+  nvm use node > /dev/null
 }
 
-svgo_activate_env() {
-  # See also: nvm use --lts
-  # - Though note --lts is not necessarily latest, e.g.,
-  #   --lts will pick v22.19.0, even when v24.7.0 installed.
-  local nvm_vers
-  if ! nvm_vers="$(nvm_latest_version)"; then
+# +++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++ #
 
-    return 1
+svgo() {
+  if ! (unset -f svgo && type -p svgo > /dev/null); then
+    svgo_prepare || return 1
   fi
 
-  nvm use ${nvm_vers} > /dev/null
+  command svgo "$@"
 }
 
 svgo_install() {
-  svgo_activate_env
+  _dxy_nvm_use_latest_node || return 1
 
   npm install -g svgo
 }
 
 svgo_prepare() {
-  svgo_activate_env || return 1
+  _dxy_nvm_use_latest_node || return 1
 
   if [ -z "$(unset -f svgo && type -p svgo)" ]; then
     >&2 echo "ERROR: Missing \`svgo\`"
@@ -81,38 +70,6 @@ svgo_prepare() {
 
     return 1
   fi
-}
-
-# +++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++ #
-
-nvm_latest_version() {
-  if ! command -v nvm > /dev/null; then
-    >&2 echo "ERROR: Missing \`nvm\`"
-    >&2 echo "- Repo not found at:"
-    >&2 echo "  ${NVM_DIR}"
-
-    return 1
-  elif test $(nvm ls v | wc -l) -le 1; then
-    # E.g.,
-    #   $ nvm ls v
-    #          v20.19.5
-    #          v22.19.0
-    #           v24.7.0
-    #   ->       system
-    >&2 echo "ERROR: Missing user-space \`npm\`"
-    >&2 echo "- Install 'em:"
-    >&2 echo "  mr -d ${DOPP_KIT:-${HOME}/.kit}/js/nvm -n installNVMNodes"
-
-    return 1
-  fi
-
-  nvm ls v --no-colors \
-    | sed 's/^\(\->\)\?\s\+//g' \
-    | sed 's/\s\+\*\?$//g' \
-    | grep -v system \
-    | sort -V \
-    | tail -1 \
-    | sed 's/^\s\+\(.*\)\( .*\)/\1/g'
 }
 
 # +++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++ #
