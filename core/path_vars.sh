61028144b2d6227bb481640feb7ead58bc029780
Landon Bouma <landonb@users.noreply.github.com>
2025-11-04 13:30:04 -0600

    refactor(lint): reformat by shfmt

--- core/path_vars.sh
+++ core/path_vars.sh
@@ -7,7 +7,7 @@
 
 # +++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++ #
 
-_dxy_export_path_var_depoxy_ambers () {
+_dxy_export_path_var_depoxy_ambers() {
   local ambers_path="${DEPOXYDIR_BASE_FULL:-${HOME}/.depoxy}/ambers"
 
   if [ -d "${DEPOXYAMBERS_DIR:-${ambers_path}}" ]; then
@@ -19,7 +19,7 @@ _dxy_export_path_var_depoxy_ambers () {
   fi
 }
 
-_dxy_export_path_var_depoxy_archetype () {
+_dxy_export_path_var_depoxy_archetype() {
   if [ -d "${DEPOXYARCHETYPE_DIR:-${DEPOXYAMBERS_DIR}/archetype}" ]; then
     export DEPOXYARCHETYPE_DIR="${DEPOXYARCHETYPE_DIR:-${DEPOXYAMBERS_DIR}/archetype}"
   elif [ -n "${DEPOXYARCHETYPE_DIR}" ]; then
@@ -29,7 +29,7 @@ _dxy_export_path_var_depoxy_archetype () {
   fi
 }
 
-_dxy_export_path_var_depoxy_projlns () {
+_dxy_export_path_var_depoxy_projlns() {
   if [ -d "${DEPOXY_PROJLNS:-${HOME}/.projlns}" ]; then
     export DEPOXY_PROJLNS="${DEPOXY_PROJLNS:-${HOME}/.projlns}"
   elif [ -n "${DEPOXY_PROJLNS}" ]; then
@@ -53,7 +53,7 @@ _dxy_export_path_var_depoxy_projlns () {
 
 # ***
 
-_dxy_export_path_var_dopp_kit () {
+_dxy_export_path_var_dopp_kit() {
   if [ -d "${DOPP_KIT:-${HOME}/.kit}" ]; then
     export DOPP_KIT="${DOPP_KIT:-${HOME}/.kit}"
   elif [ -n "${DOPP_KIT}" ]; then
@@ -63,7 +63,7 @@ _dxy_export_path_var_dopp_kit () {
   fi
 }
 
-_dxy_export_path_var_shoilerplate () {
+_dxy_export_path_var_shoilerplate() {
   if [ -d "${SHOILERPLATE:-${HOME}/.kit/sh}" ]; then
     export SHOILERPLATE="${SHOILERPLATE:-${HOME}/.kit/sh}"
   elif [ -n "${SHOILERPLATE}" ]; then
@@ -73,7 +73,7 @@ _dxy_export_path_var_shoilerplate () {
   fi
 }
 
-_dxy_export_path_var_git_basedir () {
+_dxy_export_path_var_git_basedir() {
   if [ -d "${GITREPOSPATH:-${HOME}/.kit/git}" ]; then
     export GITREPOSPATH="${GITREPOSPATH:-${HOME}/.kit/git}"
   elif [ -n "${GITREPOSPATH}" ]; then
@@ -83,7 +83,7 @@ _dxy_export_path_var_git_basedir () {
   fi
 }
 
-_dxy_export_path_var_ohmyrepos_lib () {
+_dxy_export_path_var_ohmyrepos_lib() {
   local omr_dir="${OHMYREPOS_DIR:-${GITREPOSPATH:-${HOME}/.kit/git}/ohmyrepos}"
 
   if [ -d "${omr_dir}" ]; then
@@ -98,7 +98,7 @@ _dxy_export_path_var_ohmyrepos_lib () {
 
 # +++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++ #
 
-_depoxy_path_environs () {
+_depoxy_path_environs() {
   local environ
 
   for environ in \
@@ -106,15 +106,14 @@ _depoxy_path_environs () {
     DOPP_KIT \
     SHOILERPLATE \
     GITREPOSPATH \
-    OHMYREPOS_LIB \
-  ; do
+    OHMYREPOS_LIB; do
     echo "${environ}=$(fg_mintgreen)${!environ}$(attr_reset)"
   done
 }
 
 # +++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++ #
 
-_dxy_wire_cmdpath () {
+_dxy_wire_cmdpath() {
   # Add special path variables to user's environment.
 
   # *** Export path environs.
@@ -145,11 +144,10 @@ _dxy_wire_cmdpath () {
 
 # @@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@ #
 
-main () {
+main() {
   _dxy_wire_cmdpath
   unset -f _dxy_wire_cmdpath
 }
 
 main "$@"
 unset -f main
-
