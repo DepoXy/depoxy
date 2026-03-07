23d047ab98c69e79e0de4671e2b2cec541fe8cd6
Landon Bouma <landonb@users.noreply.github.com>
2026-03-06 20:01:22 -0600

    docs(sh): add comment

--- core/aliastown.sh
+++ core/aliastown.sh
@@ -578,6 +578,7 @@ _dxy_wire_alias_wire_countdown_and_notify() {
   # MAYBE/2026-02-03: Ha, what about `alarm`? Or `alarm-countdown`?
   claim_alias_or_warn "alarm" "_dxy_countdown_and_notify"
 
+  # UCASE: Teabagging.
   claim_alias_or_warn "2m" "_dxy_countdown_and_notify 2m"
   claim_alias_or_warn "4m" "_dxy_countdown_and_notify 4m"
 }
